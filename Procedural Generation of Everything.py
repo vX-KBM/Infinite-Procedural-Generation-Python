6de48819ef8f5115b81fdc0408d9f2e3d6cf5f59
vX-KBM <36805043+vX-KBM@users.noreply.github.com>
2021-07-14 09:11:03 +0200

    Update Procedural Generation of Everything.py

--- Procedural Generation of Everything.py
+++ Procedural Generation of Everything.py
@@ -1,11 +1,8 @@
 import os
 from random import randint
-try:
-    import sys
-    if sys.platform == "win32":
-        os.system('color')
-except:
-    pass
+import sys
+if sys.platform == "win32":
+    os.system('color')
 from sty import fg, bg, ef, rs
 from sty import Style, RgbFg
 en = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
